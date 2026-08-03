77f21b68fac50377133ff4ceec6d54aa2a945b61
orscathinus <orscathinus@gmail.com>
2026-08-03 12:07:42 -0400

    Update app/who-we-are/page.tsx

--- app/who-we-are/page.tsx
+++ app/who-we-are/page.tsx
@@ -3,10 +3,9 @@ import { WhoContent } from "../components/PageContent";
 import { SiteFooter, SiteHeader } from "../components/SiteChrome";
 
 export const metadata: Metadata = { title: "Who We Are" };
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export default function WhoPage() {
   return (
-    <><SiteHeader /><WhoContent basePath={basePath} /><SiteFooter /></>
+    <><SiteHeader /><WhoContent /><SiteFooter /></>
   );
 }
