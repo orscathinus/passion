37a688aac7ec8052a9099cefc734e98f95a4ad67
orscathinus <orscathinus@gmail.com>
2026-08-03 23:58:31 -0400

    Load requested UI refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { CmsProvider } from "./components/CmsProvider";
 import "./globals.css";
+import "./overrides.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
