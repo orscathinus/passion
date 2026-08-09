ac8deb0c6485f1a800fc3b16d6e1c60c57386714
orscathinus <orscathinus@gmail.com>
2026-08-08 23:26:15 -0400

    Restore password derivation helper

---
+++ server/password.ts
@@ -0,0 +1,35 @@
+export async function derivePassword(password: string, salt: string, iterations: number) {
+  const key = await crypto.subtle.importKey(
+    "raw",
+    new TextEncoder().encode(password),
+    "PBKDF2",
+    false,
+    ["deriveBits"],
+  );
+  const bits = await crypto.subtle.deriveBits(
+    {
+      name: "PBKDF2",
+      hash: "SHA-256",
+      salt: fromBase64Url(salt),
+      iterations,
+    },
+    key,
+    256,
+  );
+
+  return toBase64Url(new Uint8Array(bits));
+}
+
+function fromBase64Url(value: string) {
+  const padded = value.replace(/-/g, "+").replace(/_/g, "/") + "=".repeat((4 - (value.length % 4)) % 4);
+  const binary = atob(padded);
+  return Uint8Array.from(binary, (character) => character.charCodeAt(0));
+}
+
+function toBase64Url(bytes: Uint8Array) {
+  let binary = "";
+  bytes.forEach((byte) => {
+    binary += String.fromCharCode(byte);
+  });
+  return btoa(binary).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
